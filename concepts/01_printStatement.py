e15c7e9497199b76e13b9f7b57c847ae013c67b4
aashishparuvada <aashishvardhanp@gmail.com>
2025-09-20 00:47:26 +0530

    printStatement

---
+++ concepts/01_printStatement.py
@@ -0,0 +1,13 @@
+print("I love London")
+print('I love Fish and Chips')
+
+# To print something on to the terminal or console, we use print statements. We can either use a single quote or double to represent a string.
+# More on strings in the strings file
+
+# You also can have string written in triple single or triple double quotes, generally used for Long paragraphs or in DOCSTRINGS (will learn in functions).
+
+print('''
+
+Hey I am using the triple quotes.
+
+''')
